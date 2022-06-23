60cc2d3124fbef2fd55af1f6c7306179f159c080
jofurdz <jofurdz@gmail.com>
2022-06-23 15:50:39 -0500

    task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,27 +6,10 @@ import sys
 
 def minOperations(n):
     """minimum operations to find nH characters in file"""
-    dp = [sys.maxsize] * (n + 1)
-    # Initial state
-    dp[1] = 0
-    # Iterate for the remaining numbers
-    for i in range(2, n + 1):
-        # If the number can be obtained
-        # by multiplication by 2
-        if i % 2 == 0:
-            x = dp[i // 2]
-            if x + 1 < dp[i]:
-                dp[i] = x + 1
-        # If the number can be obtained
-        # by multiplication by 3
-        if i % 3 == 0:
-            x = dp[i // 3]
-            if x + 1 < dp[i]:
-                dp[i] = x + 1
-        # Obtaining the number by adding 1
-        x = dp[i - 1]
-        if x + 1 < dp[i]:
-            dp[i] = x + 1
-    # Return the minimum operations
-    # to obtain n
-    return dp[n]
+    minOp, div = 0, 2
+    while isinstance(n, int) and n > 1:
+        while n % div:
+            div += 1
+        minOp += div
+        n = int(n / div)
+    return minOp
